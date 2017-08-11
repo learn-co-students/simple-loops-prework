d04eee1bebef8b480d7905d95fd2f518590098f3
Todd Greenberg <tsgreenberg1217@gmail.com>
2017-08-11 04:57:32 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -23,9 +23,8 @@ end
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   number_of_times.times do
-    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts "Welcome to Flatiron School's Web Development Course!"
   end
-
 end
 
 def while_iterator(number_of_times)
@@ -64,4 +63,4 @@ def for_iterator(number_of_times)
 
 
 end
-puts loop_iterator(7)
+puts times_iterator(7)
