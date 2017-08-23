52d3df732bcd928ff38fe16bacf646adf84a75a8
Ashe Austaire <AsheAustaire@gmail.com>
2017-08-23 13:50:09 -0400

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,41 +1,57 @@
-# REMEMBER: print your output to the terminal using 'puts'
+def loop_iterator(num)
 
-def loop_iterator(number_of_times)
-    phrase = "Welcome to Flatiron School's Web Development Course!"
-    number_of_times.times do
-        puts phrase
-    end
-end
-
-def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times.times do
-      puts phrase
-  end
-end
-
-def while_iterator(number_of_times)
     counter = 0
-    phrase = "Welcome to Flatiron School's Web Development Course!"
-    while counter < number_of_times
-    puts phrase
-    counter += 1
+
+    loop do
+
+        puts 'Welcome to Flatiron School\'s Web Development Course!'
+        #--
+        counter += 1
+        break if counter == num
+
     end
 end
 
-def until_iterator(number_of_times)
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  counter = 0
-  until counter == number_of_times
-      puts phrase
-      counter += 1
-  end
+def times_iterator(num)
+
+    num.times do
+
+        puts 'Welcome to Flatiron School\'s Web Development Course!'
+        #--
+    end
 end
 
-def for_iterator(number_of_times)
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  for i in 1..number_of_times
-      puts phrase
-  end
+def while_iterator(num)
+
+    counter = 0
+
+    while counter < num
+
+        puts 'Welcome to Flatiron School\'s Web Development Course!'
+        #--
+        counter += 1
+    end
+end
+
+def until_iterator(num)
+
+    counter = 0
+
+    until counter == num
+
+    puts 'Welcome to Flatiron School\'s Web Development Course!'
+    #--
+    counter += 1
+
+    end
+end
+
+def for_iterator(num)
+
+    for n in (0...num)
+
+    puts 'Welcome to Flatiron School\'s Web Development Course!'
+    #--
+
+    end
 end
