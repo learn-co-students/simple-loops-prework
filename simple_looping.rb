ac6198c8b44fbee37c860252b8f7c0e74e794310
Laura Adam <laurawynosky@gmail.com>
2018-05-30 00:36:10 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,5 +1,5 @@
-# REMEMBER: print your output to the terminal using 'puts'
-
+learn# REMEMBER: print your output to the terminal using 'puts'
+#1
 def loop_iterator(number_of_times)
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
@@ -9,34 +9,55 @@ def loop_iterator(number_of_times)
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
-end
+  counter = 0 
+  loop do
+    counter +=1
+    puts phrase
+    if counter >=10 
+      break
+    end
 
+#2 X
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
+   
+	10.times do
+		puts phrase
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  end
 end
 
+#3 X
 def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
+  number_of_times =0
+  while number_of_times< 10
+  puts phrase
+  number_of_times+=1 
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  end
 end
 
+#4 X
 def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
+  number_of_times =0
+  until number_of_times == 10
+  puts phrase
+  number_of_times  +=1 
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  end
 end
-
+  
+#5 X
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  for counter in 1..10 do
+  puts phrase
+  number_of_times+=1 
+  end
 end
-
