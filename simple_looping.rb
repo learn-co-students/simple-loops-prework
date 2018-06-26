fbd971fa1e2f56062e6fecc05106f8a18f46e718
Rosa Tu <rosalina.tu@gmail.com>
2018-06-26 22:52:04 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -27,9 +27,9 @@ end
 
 def until_iterator(number_of_times)
   counter = 0
+  puts "Welcome to Flatiron School's Web Development Course!"
   counter += 1
   until interation == number_of_times
-  puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
 
