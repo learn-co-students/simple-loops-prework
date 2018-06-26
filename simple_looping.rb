9f0d87ab4a77c79e60b3412bb791145fcfec35ce
Rosa Tu <rosalina.tu@gmail.com>
2018-06-26 23:38:05 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -8,17 +8,6 @@ def loop_iterator(number_of_times)
   end
 end  
 
-def loop_iterator(number_of_times)
-  counter = 0
-  loop do 
-    puts "Welcome to Flatiron School's Web Development Course!"
-   counter += 1  
-    if counter > number_of_times
-  break
-  end
-  end
-end 
-
 def times_iterator(number_of_times)
   number_of_times.times do
     puts "Welcome to Flatiron School's Web Development Course!"
