51a0af39066dfcb02a1b7402362b0fe16470dbce
Irfan Suleman <ivsuleman@gmail.com>
2018-09-16 17:03:14 +0100

    fixed2

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,5 +1,14 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
+
+
+
+
+
+
+
+
+
 def loop_iterator(number_of_times)
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
