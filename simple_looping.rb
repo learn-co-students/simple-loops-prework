fad4742ab0b8d2ed8a6d0dfa054f3b25279038d1
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-10-29 03:17:27 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,37 +10,49 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
   counter = 0  
   loop do
-    phrase = "Welcome to Flatiron School's Web Development Course!"
     counter += 1
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
     break if number_of_times == counter
   end
 end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
+  number_of_times.times do
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+    puts phrase
+  end
 end
 
 def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  counter = 0
+  while counter < number_of_times
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
+    counter += 1
+  end
 end
 
 def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-
+  counter = 0
+  until counter == number_of_times
+    counter += 1
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
+  end
 end
 
 def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
-  # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  # remember that `for` requires a range of numbers. How can we turn our number into a range? 
+  for counter in 1..number_of_times do
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
+  end
 end
 
