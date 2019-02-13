ab396b32624462e5a04e9aaff65e8914ee88db31
ko kristin <koberlander115@gmail.com>
2019-02-13 21:53:34 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,10 +11,6 @@ loop do
   end
 end
 
-
-  
-end
-
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
