22c3ce02e574947d9c9d80d68de4cec35431ccd0
errolgh <ewatsonrei@gmail.com>
2019-02-27 18:51:46 -0500

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -4,27 +4,25 @@ def loop_iterator(number_of_times)
   loop do
     puts "#{phrase}"
     counter += 1
-    if counter == number_of_times
-      break
-    end
+    break if counter == number_of_times
   end
-end
+end #most lines
 
 def times_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   number_of_times.times do
   puts "#{phrase}"
-  end 
-end
+  end
+end #most human readable
 
 def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-counter = 0
+  counter = 0
   while counter < number_of_times
     puts "#{phrase}"
     counter += 1
   end
-end
+end #does not use "==" comparison operator
 
 def until_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
@@ -37,8 +35,8 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  counter = 0
-  for counter in 1..number_of_times
+  counter = 1..number_of_times
+  for item in counter
     puts "#{phrase}"
   end
-end
+end #most abstraction, tied for most terse
