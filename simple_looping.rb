bb293ef23bdab626ba3f3958a08650bb2bcca6c3
Tess <tessneau@gmail.com>
2019-05-12 04:47:12 -0400

    done

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,6 +11,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
 
+
   loop do
     puts phrase
     counter += 1
@@ -33,7 +34,7 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
 
-  while counter < number_of_times 
+  while counter < number_of_times
     puts phrase
     counter += 1
   end
